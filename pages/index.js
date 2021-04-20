490640d20f5c81c7bddbb4cab254f7931b210bcc
danwakeem <danwakeem@gmail.com>
2021-04-20 17:44:13 -0500

    Update message

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main className="container mx-auto">
-        <Hero title="hi daddy" />
+        <Hero title="hi brother man" />
       </main>
     </div>
   )
